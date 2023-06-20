f0b9f0c08d7735cfb000372c9e1d69b2dc230ea1
Codeman <top.talented@outlook.com>
2023-06-21 03:03:24 +0800

    feat: add user, book resources

---
+++ app/Http/Resources/BooksResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BooksResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (string) $this->id,
+            'title'  => $this->title,
+            'description' => $this->description,
+            'publisher' => $this->publisher,
+            'author' => $this->author,
+            'cover_photo' => $this->cover_photo,
+            'price' => $this->price,
+        ];
+    }
+}
